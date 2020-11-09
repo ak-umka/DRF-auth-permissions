8aeab961225ac1f0a6f3758065c72f2ae35eb999
ak-umka <umbet70@gmail.com>
2020-11-09 13:09:26 +0600

    first commit

--- core/models.py
+++ core/models.py
@@ -1,35 +1,15 @@
 from django.db import models
 from datetime import datetime, date
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
-class Car(models.Model):
-    vendor = models.CharField(max_length=128)
-    model = models.CharField(max_length=128)
-    year = models.PositiveSmallIntegerField()
-    volume = models.PositiveIntegerField()
 
 
-class Fighter(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    date_of_birth= models.DateTimeField(null=True)
-    age = models.PositiveSmallIntegerField(default=0)
-    weight = models.FloatField(null=True)
-    growth = models.FloatField(null=True)
 
-class Federation(models.Model):
-    options = (
-        ('Martial arts','Martial arts   '),
-        ('Full contact sports','Full contact sports'),
-        ('MMA','MMA'),
-        ('Karate','Karate'),
-        ('Applied arts','Applied arts'),
-    )
-    Federation = models.CharField(max_length=20, choices=options)
-    directions = models.CharField(max_length=50, default='SOME STRING')
 
-class Tag(models.Model):
-    name = models.CharField(max_length=100, unique=True)
 
 class News(models.Model):
     title = models.CharField(max_length=100)
@@ -37,3 +17,4 @@ class News(models.Model):
     author = models.CharField(max_length=40)
     image = models.ImageField(null=True, blank=True)
     publication_date = models.DateField(null=True)
+    owner = models.ForeignKey('auth.User', related_name='news', on_delete=models.CASCADE)
--- core/serializers.py
+++ core/serializers.py
@@ -1,23 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
-
 from core.models import Car, Fighter, Federation, News
+from django.contrib.auth.models import User
 
 
-class CarSerializer(ModelSerializer):
+class UserSerializer(ModelSerializer):
+    news = serializers.PrimaryKeyRelatedField(many=True, queryset = News.objects.all)
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
-        model = Car
+        model = User
         fields = "__all__"
 
-class FighterSerializer(ModelSerializer):
-    class Meta:
-        model = Fighter
-        fields = "__all__"
-
-class FederationSerializer(ModelSerializer):
-    class Meta:
-        model = Federation
-        fields = "__all__"
 
 class NewsSerializer(ModelSerializer):
     publication_date = serializers.DateField(format="%d-%m-%Y", input_formats=['%d-%m-%Y', 'iso-8601'])
@@ -25,3 +18,26 @@ class NewsSerializer(ModelSerializer):
         model = News
         fields = "__all__"
 
+class LoginSerializer():
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        username = data.get("username", "")
+        password = data.get("password", "")
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user:
+                if user.is_active:
+                    data["user"] = user
+                else:
+                    msg = "user is deactivated"
+                    raise exceptions.ValidationError(msg)
+            else:
+                msg = "Unable to login with given credentials."
+                raise exceptions.ValidationError(msg)
+        else:
+            msg = "Must provide username and password both."
+            raise exceptions.ValidationError(msg)
+        return data
